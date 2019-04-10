66f8d4c941922b469bdecae6b9775c15056f1cfa
nontoa <nicolasnc010@gmail.com>
2019-04-10 14:56:54 -0500

    Parte2

--- HTML5_JS_STOMP_WebSockets-master/src/main/resources/static/app.js
+++ HTML5_JS_STOMP_WebSockets-master/src/main/resources/static/app.js
@@ -1,15 +1,15 @@
 var app = (function () {
 
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
     }
-    
+
     var stompClient = null;
 
-    var addPointToCanvas = function (point) { 
+    var addPointToCanvas = function (point) {
         //console.log(point);      
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
@@ -17,8 +17,8 @@ var app = (function () {
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
-    
+
+
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
@@ -31,7 +31,7 @@ var app = (function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-        
+
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
@@ -41,26 +41,32 @@ var app = (function () {
                 //console.log(npoint2+"------------------")
                 alert("Si funciono!!");
                 addPointToCanvas(npoint);
-                
+
             });
         });
 
     };
-    
-    
+
+
 
     return {
-        
+
         init: function () {
             var can = document.getElementById("canvas");
-            
+            can.addEventListener("click", function (evt) {
+                var coord = getMousePosition(evt);
+                let px = coord.x;
+                let py = coord.y;
+                var point = new Point(px, py);
+                app.publishPoint(px,py);
+            })
             //websocket connection
             connectAndSubscribe();
         },
 
-        publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            console.info("publishing point at "+pt);
+        publishPoint: function (px, py) {
+            var pt = new Point(px, py);
+            console.info("publishing point at " + pt);
             //addPointToCanvas(pt);
 
             //publicar el evento
