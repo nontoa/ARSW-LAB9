56b9994982bd3f910eaa06f4a933434d7e9f9798
unknown <2121990@labinfo.is.escuelaing.edu.co>
2019-04-10 11:36:00 -0500

    Parte1

---
+++ HTML5_JS_STOMP_WebSockets-master/src/main/resources/static/app.js
@@ -0,0 +1,80 @@
+var app = (function () {
+
+    class Point{
+        constructor(x,y){
+            this.x=x;
+            this.y=y;
+        }        
+    }
+    
+    var stompClient = null;
+
+    var addPointToCanvas = function (point) { 
+        //console.log(point);      
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+        ctx.stroke();
+    };
+    
+    
+    var getMousePosition = function (evt) {
+        canvas = document.getElementById("canvas");
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    };
+    var connectAndSubscribe = function () {
+        console.info('Connecting to WS...');
+        var socket = new SockJS('/stompendpoint');
+        stompClient = Stomp.over(socket);
+        
+        //subscribe to /topic/TOPICXX when connections succeed
+        stompClient.connect({}, function (frame) {
+            console.log('Connected: ' + frame);
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                var npoint = JSON.parse(eventbody.body);
+                //var npoint2 = new Point(npoint.data.x,npoint.data.y)
+                //console.log(npoint2+"------------------")
+                alert("Si funciono!!");
+                addPointToCanvas(npoint);
+                
+            });
+        });
+
+    };
+    
+    
+
+    return {
+        
+        init: function () {
+            var can = document.getElementById("canvas");
+            
+            //websocket connection
+            connectAndSubscribe();
+        },
+
+        publishPoint: function(px,py){
+            var pt=new Point(px,py);
+            console.info("publishing point at "+pt);
+            //addPointToCanvas(pt);
+
+            //publicar el evento
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
+
+        },
+
+        disconnect: function () {
+            if (stompClient !== null) {
+                stompClient.disconnect();
+            }
+            setConnected(false);
+            console.log("Disconnected");
+        }
+    };
+
+})();
